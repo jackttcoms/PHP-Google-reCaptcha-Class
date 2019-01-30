ed4865766e81652e24940492489281d6a484c7a9
jackttcoms <jack@ttcoms.com>
2019-01-30 01:15:32 +0000

    Update example.php

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ $reCaptcha = new reCaptcha('register', '(site key)', '(secret key)'); // load re
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') // Check form submitted via POST
 {
-	$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 	
 	if (!$reCaptcha->success()) {
 		echo 'recaptcha failed!';
