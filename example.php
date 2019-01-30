cdcfbe8af11bc361d6f9a36c1317275a76f65b76
jackttcoms <jack@ttcoms.com>
2019-01-30 01:12:11 +0000

    Update example.php

--- example.php
+++ example.php
@@ -1,14 +1,15 @@
+<?php
 // call your class (require_once)
 
 $reCaptcha = new reCaptcha('register', '(site key)', '(secret key)'); // load recaptcha class for spam protection
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') // Check form submitted via POST
 {
-		$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-		
-		if (!$reCaptcha->success()) {
+	$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+	
+	if (!$reCaptcha->success()) {
     		$data['formData']['global_err'] = 'recaptcha failed!';
-    }
+    	}
 } else {
 		// other stuff
 }
